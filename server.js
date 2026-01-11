f97d4b7eae40743fc4779f951ad128adbf60614e
Muhammad Saqib <113930907+saqibhakeem@users.noreply.github.com>
2026-01-11 18:37:34 +0000

    vercel compatible

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ dotenv.config();
 
 import app from "./src/app.js";
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
+export default app
